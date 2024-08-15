232ab2dcb8bb039517a073d588bbdfecd6a70822
Alphonse <alphonsemehounme9@gmail.com>
2024-08-15 16:52:16 +0000

    float to int function

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,4 +5,7 @@ String concat module
 
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Concats two strings
+    """
     return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Floor function module
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float
+    """
+    return int(n)
