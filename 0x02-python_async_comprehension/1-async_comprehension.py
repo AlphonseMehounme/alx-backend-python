bf0b59fe76255f5c3b52bfa94175e2edf30eb375
Alphonse <alphonsemehounme9@gmail.com>
2024-08-21 09:22:35 +0000

    Add return type annotation to async_comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,12 +3,13 @@
 Async comprehension
 """
 import asyncio
+from typing import Generator
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> Generator[float, None, None][]:
     """
     Async comprehension with async generator
     """
