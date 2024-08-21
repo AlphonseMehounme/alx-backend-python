bcad13ae20dba21af66e8596b298027bbe62af47
Alphonse <alphonsemehounme9@gmail.com>
2024-08-21 09:36:45 +0000

    Change async_comprehension return type and add to measure_runtime

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,13 +3,13 @@
 Async comprehension
 """
 import asyncio
-from typing import Generator
+from typing import List
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None][]:
+async def async_comprehension() -> List[float]:
     """
     Async comprehension with async generator
     """
